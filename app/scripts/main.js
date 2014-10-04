52524cf9d45290d24f965188c3b4bfaa5734ebbf
eli <eli@EliMBPR.local>
2014-10-04 15:01:12 -0400

    basic event structure working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+(function(){
+	'use strict';
+	
+	var pressTimer;
+	var pressCounter = 0;
+
+	window.addEventListener('touchstart',function(event){
+		window.startX = event.pageX;
+		window.startY = event.pageY;
+
+		pressTimer = setInterval(function(){
+			//650ms taphold event
+			if(pressCounter >= 65) {
+				alert('open nav');
+				clearInterval(pressTimer);
+				pressCounter = 0;
+			} else {
+				pressCounter ++;
+			}
+	    },10);
+	},false);
+
+	window.addEventListener('touchmove',function(event){
+    	clearInterval(pressTimer);
+    	pressCounter = 0;
+	},false);
+
+})();
