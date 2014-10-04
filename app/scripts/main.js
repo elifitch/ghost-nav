82a0c1135c8f4ec4be20a138fc0b9d1da576da31
eli <eli@EliMBPR.local>
2014-10-04 18:03:08 -0400

    some progress on radial menu styling etc

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,7 +11,8 @@
 		pressTimer = setInterval(function(){
 			//650ms taphold event
 			if(pressCounter >= 65) {
-				alert('open nav');
+				// alert('open nav');
+				openNav();
 				clearInterval(pressTimer);
 				pressCounter = 0;
 			} else {
@@ -24,5 +25,21 @@
     	clearInterval(pressTimer);
     	pressCounter = 0;
 	},false);
+
+	function openNav(){
+		var items = document.querySelectorAll('.circle a');
+
+		for(var i = 0, l = items.length; i < l; i++) {
+		  items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+		  
+		  items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+		}
+
+		// document.querySelector('.menu-button').onclick = function(e) {
+		   // e.preventDefault(); 
+		   document.querySelector('.circle').classList.toggle('open');
+		// }
+
+	};
 
 })();
